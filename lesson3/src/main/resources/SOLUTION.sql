c878b903f2756e7fc2fd56651bd24b8a9710641b
raman <r4m4n911@gmail.com>
2025-10-22 00:15:21 +0300

    modified:   lesson3/src/main/resources/SOLUTION.sql 	modified:   lesson5/src/main/resources/SOLUTION.sql 	modified:   lesson6/src/main/resources/SOLUTION.sql 	modified:   lesson7/src/main/resources/SOLUTION.sql 	modified:   lesson8/src/main/resources/SOLUTION.sql

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,29 +1,15 @@
-update student
-set birthday = '2001-09-11'
-where birthday is null;
-alter table student
-    alter column birthday set not null;
+update student set birthday = '2001-09-11' where birthday is null;
+alter table student alter column birthday set not null;
 
-alter table mark
-    add constraint mark_check check (mark >= 1 and mark <= 10),
-alter column student_id set not null,
-    alter column subject_id set not null;
+alter table mark add constraint mark_check check (mark >= 1 and mark <= 10), alter column student_id set not null, alter column subject_id set not null;
 
-alter table subject
-    add constraint grade_check check (grade >= 1 and grade <= 5);
+alter table subject add constraint grade_check check (grade >= 1 and grade <= 5);
 
-alter table paymenttype
-    add unique(name);
+alter table paymenttype add unique(name);
 
-delete from payment
-where type_id is null;
-update payment
-set amount = 0
-where type_id is null;
-update payment
-set payment_date = '2001-09-11'
-where payment_date is null;
-alter table payment
-    alter column type_id set not null,
+delete from payment where type_id is null;
+update payment set amount = 0 where type_id is null;
+update payment set payment_date = '2001-09-11' where payment_date is null;
+alter table payment alter column type_id set not null,
 alter column amount set not null,
 alter column payment_date set not null;
--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,27 +1,18 @@
 
 
-select * from mark
-where Mark.mark > 6
-order by mark desc;
+select * from mark where Mark.mark > 6 order by mark desc;
 
 
-select * from payment
-where amount < 300
-order by amount asc;
+select * from payment where amount < 300 order by amount asc;
 
 
-select * from paymenttype
-order by name desc;
+select * from paymenttype order by name desc;
 
 
-select * from student
-order  by name desc;
+select * from student order  by name desc;
 
 
 
-select * from student
-where id in (select student_id from payment
-             where amount > 1000)
-order by birthday;
+select * from student where id in (select student_id from payment where amount > 1000) order by birthday;
 
 
